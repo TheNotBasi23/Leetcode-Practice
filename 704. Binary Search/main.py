6bd40a7e9462b767d29a7467966a8ec0c1508ba6
TheNotBasi23 <ksv.kris@hotmail.com>
2026-01-22 03:54:21 +0000

    recompleted the binary search except this time im not cheating with libraries

--- 704. Binary Search/main.py
+++ 704. Binary Search/main.py
@@ -1,15 +1,22 @@
-import bisect
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        index = bisect.bisect_left(nums,target)
-        if 0 <= index <len(nums):
-            if nums[index] == target:
-                return index
+        leftPointer = 0
+        rightPointer = len(nums) - 1
+        while leftPointer < rightPointer:
+            mid = leftPointer + (rightPointer - leftPointer) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                leftPointer = mid + 1
             else:
-                return -1
+                rightPointer = mid - 1
+        if nums[leftPointer] == target:
+            return leftPointer
         else:
             return -1
 
+
+
 temp = Solution()
-print(temp.search(nums = [-1,0,3,5,9,12], target = 2))
+print(temp.search(nums = [-1,0,3,5,9,12], target = 0))
 
