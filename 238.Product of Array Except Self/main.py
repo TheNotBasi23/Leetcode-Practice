3fa2886421d5e29d51f81f09ca6b106c7ba7ae37
TheNotBasi23 <ksv.kris@hotmail.com>
2026-01-25 06:02:31 +0000

    tried this challenge again. I have got the hang of it now

--- 238.Product of Array Except Self/main.py
+++ 238.Product of Array Except Self/main.py
@@ -2,22 +2,39 @@ from typing import List
 
 
 class Solution:
+    """
+
+        def productExceptSelf(self, nums: List[int]) -> List[int]:
+            start = 1
+            ps = []
+            for num in nums:
+                start = start * num
+                ps.append(start)
+            start = 1
+            nums.reverse()
+            reversePS = []
+            for num in nums:
+                start = start * num
+                reversePS.append(start)
+            reversePS.reverse()
+            nums.reverse()
+            n = len(nums)
+            return [((ps[i - 1]) if 0 <i < n else 1) * ((reversePS[i + 1]) if 0 <= i < n - 1 else 1) for i in range(len(nums))]
+
+
+    """
+
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        start = 1
-        ps = []
-        for num in nums:
-            start = start * num
-            ps.append(start)
-        start = 1
-        nums.reverse()
-        reversePS = []
-        for num in nums:
-            start = start * num
-            reversePS.append(start)
-        reversePS.reverse()
-        nums.reverse()
+        pp = []
+        rpp = [0] * len(nums)
         n = len(nums)
-        return [((ps[i - 1]) if 0 <i < n else 1) * ((reversePS[i + 1]) if 0 <= i < n - 1 else 1) for i in range(len(nums))]
+        for num in nums:
+            pp.append(num * (pp[-1] if pp else 1))
+        for i in range(len(rpp) - 1, - 1, -1):
+            rpp[i] = nums[i] * (rpp[i+1] if i+1 < n else 1)
+        return [(pp[i-1] if i-1 >= 0 else 1) * (rpp[i+1] if i + 1 < n else 1) for i in range(n)]
+
+
 
 
 #nums = [1,2,3,4]
