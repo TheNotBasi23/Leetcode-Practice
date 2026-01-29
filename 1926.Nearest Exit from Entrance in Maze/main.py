8e778e26299ef19082bda4b613ce00ee01e0b292
TheNotBasi23 <ksv.kris@hotmail.com>
2026-01-29 12:10:49 +0000

    done some optimisation

--- 1926.Nearest Exit from Entrance in Maze/main.py
+++ 1926.Nearest Exit from Entrance in Maze/main.py
@@ -6,7 +6,6 @@ class Solution:
         exits = set()
         n = len(maze)
         m = len(maze[0])
-        visited = set()
         for i in range(m):
             if maze[0][i] == ".":
                 exits.add((0,i))
@@ -15,7 +14,6 @@ class Solution:
         for i in range(1,len(maze) - 1):
             if maze[i][0] == ".":
                 exits.add((i,0))
-            print(n-1)
             if maze[i][m - 1] == ".":
                 exits.add((i,m-1))
         if (entrance[0],entrance[1]) in exits:
@@ -26,18 +24,18 @@ class Solution:
         count = 0
         dir = [(0,1),(1,0),(-1,0),(0,-1)]
         itemsToProcess = len(q)
-        visited.add((entrance[0],entrance[1]))
+        maze[entrance[0]][entrance[1]] = "+"
         layer = 0
         while q:
             itemL,itemR = q.popleft()
             if (itemL,itemR) in exits:
                 return layer
             count +=1
-            potenitalCandidates = [(itemL + l,itemR + r) for l,r in dir if 0 <= itemL + l < n and 0<= itemR + r < m  and (itemL + l,itemR + r) not in visited and maze[itemL + l][itemR + r] == "." ]
+            potenitalCandidates = [(itemL + l,itemR + r) for l,r in dir if 0 <= itemL + l < n and 0<= itemR + r < m  and maze[itemL + l][itemR + r] == "." ]
 
             for element in potenitalCandidates:
                 q.append(element)
-                visited.add(element)
+                maze[element[0]][element[1]] = "+"
             if count == itemsToProcess:
                 itemsToProcess = len(q)
                 count = 0
