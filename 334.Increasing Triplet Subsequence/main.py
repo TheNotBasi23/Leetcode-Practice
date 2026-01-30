5c2b83c7a692991f36e8d58960e433874a359ad7
TheNotBasi23 <ksv.kris@hotmail.com>
2026-01-30 12:18:45 +0000

    done

--- 334.Increasing Triplet Subsequence/main.py
+++ 334.Increasing Triplet Subsequence/main.py
@@ -1,3 +1,12 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        stack = []
+        smallestNum = float('inf')
+        secondSmallestNum = float('inf')
+        for num in nums:
+            if num <= smallestNum:
+                smallestNum = num
+            elif num <= secondSmallestNum:
+                secondSmallestNum = num
+            else:
+                return True
+        return False
